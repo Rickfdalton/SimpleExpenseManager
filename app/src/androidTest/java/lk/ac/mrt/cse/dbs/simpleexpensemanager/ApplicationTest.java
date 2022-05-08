83d0f934fa07003167c38dfc7baca028bcfdcc94
Jathavan Mahendrarajah <73867299+Rickfdalton@users.noreply.github.com>
2022-05-08 11:05:10 +0530

    Update ApplicationTest.java

--- app/src/androidTest/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/ApplicationTest.java
+++ app/src/androidTest/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/ApplicationTest.java
@@ -44,8 +44,8 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
     public void testAddAccount() {
         db.addAccount("android_test_4","android_test_4","android_test_4",100,1);
         Account acc = db.getAccount("android_test_4");
-//         assertTrue(acc!=null);
-           assertTrue(false);
+        assertTrue(acc!=null);
+
     }
 
     public void testGetAccount() {
